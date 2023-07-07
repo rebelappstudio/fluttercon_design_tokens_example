8fe2b87a4be982e36b6275ea3fcf4d031df7646b
Timo Pieti <timo.pieti@codemate.com>
2023-07-07 11:38:42 +0200

    Add page container component

--- lib/views/chats_view.dart
+++ lib/views/chats_view.dart
@@ -1,4 +1,3 @@
-import 'package:app_theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttercon_design_tokens_example/data/data.dart';
 import 'package:fluttercon_design_tokens_example/widgets/widgets.dart';
@@ -15,33 +14,11 @@ class _ChatsViewState extends State<ChatsView> {
 
   @override
   Widget build(BuildContext context) {
-    final customTextStyles = AppTypography.fromContext(context);
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(
-            left: AppSpacing.normal,
-            right: AppSpacing.normal,
-            bottom: AppSpacing.small,
-          ),
-          child: Row(
-            children: [
-              Text(
-                'Recent messages',
-                style: customTextStyles.heading2,
-                textAlign: TextAlign.start,
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: ListView(
-            children: chatItems
-                .map((e) => ChatListItem(chatItem: e, onTap: _onItemSelected))
-                .toList(),
-          ),
-        ),
-      ],
+    return PageContainer(
+      title: 'Recent messages',
+      children: chatItems
+          .map((e) => ChatListItem(chatItem: e, onTap: _onItemSelected))
+          .toList(),
     );
   }
 }
